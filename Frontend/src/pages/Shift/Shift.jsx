58cb7305158603f2df4293d4c04b18f6928826a5
unknown <jameskagunga2022@gmail.com>
2024-04-19 04:36:15 -0700

    started intergration with frontend

--- Frontend/src/pages/Shift/Shift.jsx
+++ Frontend/src/pages/Shift/Shift.jsx
@@ -1,56 +1,146 @@
 import "./shift.css";
+import { Link, useLocation } from "react-router-dom";
 import { FaArrowLeft, FaPlus } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { publicRequest } from "../../requestMethods";
 const Shift = () => {
+  const [shift, setShift] = useState({});
+  const location = useLocation();
+  const [coords, setCoords] = useState({});
+  const [accuracy, setAccuracy] = useState(0);
+  const [currentTime, setCurrentTime] = useState(
+    new Date().toLocaleTimeString()
+  );
+  const shiftId = location.pathname.split("/")[2];
+
+  useEffect(() => {
+    const getShift = async () => {
+      try {
+        const res = await publicRequest("/shifts/find/" + shiftId);
+
+        setShift(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getShift();
+  }, [shiftId]);
+
+  const handleClockIn = async () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude, accuracy } = position.coords;
+          const coords = { lat: latitude, lng: longitude };
+          setCoords(coords);
+          setAccuracy(accuracy);
+          setCurrentTime(new Date().toLocaleTimeString());
+        },
+
+        (error) => {
+          console.error("Error getting location", error);
+        }
+      );
+
+      try {
+        if (accuracy && coords && currentTime) {
+          await publicRequest.put(`/shifts/clockin/${shiftId}`, {
+            time: currentTime,
+            coords,
+            accuracy,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }else{
+      console.error("Geolocation is not supported by this browser");
+    }
+  };
+
+  const handleClockOut = async () => {
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude, accuracy } = position.coords;
+          const coords = { lat: latitude, lng: longitude };
+          setCoords(coords);
+          setAccuracy(accuracy);
+          setCurrentTime(new Date().toLocaleTimeString());
+        },
+
+        (error) => {
+          console.error("Error getting location", error);
+        }
+      );
+
+      try {
+        if (accuracy && coords && currentTime) {
+          await publicRequest.put(`/shifts/clockout/${shiftId}`, {
+            time: currentTime,
+            coords,
+            accuracy,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }else{
+      console.error("Geolocation is not supported by this browser");
+    }
+  };
+
   return (
     <div className="shift-container">
       <div className="shift">
         <span className="myshifts_back">
-          <FaArrowLeft /> Back
+          <Link to="/staffs">
+            <FaArrowLeft /> Back
+          </Link>
         </span>
 
         <div className="shift_details">
           <ul>
             <li>
               <strong>ID:</strong>
-              65e7963376a621ce77cfe984
+              {shift._id}
             </li>
             <li>
               <strong>Location:</strong>
-              123 Smith Street Sydney, NSW 2000 Australia
+              {shift.location}
             </li>
             <li>
               <strong>Date and Time:</strong>
-              15/03/2024 08:00 AM - 11: 00 AM
+              {shift.date} {shift.time}
             </li>
-            <li>
-              <strong>Date and Time:</strong>
-              15/03/2024 08:00 AM - 11: 00 AM
-            </li>
+
             <li>
               <strong>Type:</strong>
-              AM
+              {shift.type}
             </li>
             <li>
-              <strong>Duration:</strong>3 hours
+              <strong>Duration:</strong>
+              {shift.duration}
             </li>
 
             <li>
               <strong>Client:</strong>
-              Alok Mondala
+              {shift.client}
             </li>
             <li>
               <strong>Status:</strong>
-              Pending
+              {shift.status}
             </li>
             <li>
               <strong>Notes:</strong>
-              Make sure to clean room.
+              {shift.notes}
             </li>
           </ul>
 
           <div className="distance">
             <strong>Distance Covered</strong>
-            <input type="Number" placeholder="10" />
+            <input type="Number" placeholder={shift.distance} />
 
             <div className="distance-update">
               <span>km</span>
@@ -86,16 +176,14 @@ const Shift = () => {
             <button>Submit</button>
           </div>
         </div>
-
-        
       </div>
       <div className="button-container">
-          <button className="shift_report_btn">Report</button>
-          <div className="clockin_clockout">
-            <button className="shift_clockin_btn">Clock In</button>
-            <button className="shift_clockout_btn">Clock Out</button>
-          </div>
+        <button className="shift_report_btn">Report</button>
+        <div className="clockin_clockout">
+          <button className="shift_clockin_btn">Clock In</button>
+          <button className="shift_clockout_btn">Clock Out</button>
         </div>
+      </div>
     </div>
   );
 };
--- Frontend/src/pages/Staff/Staff.jsx
+++ Frontend/src/pages/Staff/Staff.jsx
@@ -1,14 +1,58 @@
 import { FaUser } from "react-icons/fa";
 import { AiOutlineEye } from "react-icons/ai";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./staff.css";
-import { useState } from "react";
+import moment from "moment";
+import { useState, useEffect } from "react";
+import { publicRequest } from "../../requestMethods";
+
 const Staff = () => {
   const [profile, setProfile] = useState(false);
+  const [data, setData] = useState([]);
+  const [unassignedShifts, setUnassignedShifts] = useState([]);
 
   const handleProfile = () => {
     setProfile(!profile);
   };
+
+  useEffect(() => {
+    const getShifts = async () => {
+      try {
+        const res = await publicRequest.post("/shifts/me", {
+          email: "alokmondala199@gmail.com",
+        });
+        setData(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getShifts();
+  }, []);
+
+  useEffect(() => {
+    const getShifts = async () => {
+      try {
+        const res = await publicRequest.get("/shifts/unassign");
+        setUnassignedShifts(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getShifts();
+  }, []);
+
+  const formatDate = (date) => {
+    let parsedDate = moment(date);
+
+    // Format the parsed date to "Fri 23"
+    let formattedDate = parsedDate.format("ddd DD");
+
+    return formattedDate;
+  };
+
   return (
     <div className="staff">
       <div className="stafftop">
@@ -22,9 +66,13 @@ const Staff = () => {
 
           {profile && (
             <div className="staff_account">
-              <Link to="/myaccount"><span>My Account</span></Link>
+              <Link to="/myaccount">
+                <span>My Account</span>
+              </Link>
               <span>My Statements</span>
-              <Link to="/myshifts"><span>My Shifts</span></Link>
+              <Link to="/myshifts">
+                <span>My Shifts</span>
+              </Link>
               <span>Report Incidence</span>
               <span>Logout</span>
             </div>
@@ -35,71 +83,56 @@ const Staff = () => {
       <div className="staff_main">
         <h3 className="shift_header">My Shifts</h3>
 
-        <div className="staff_main_card">
-          <div className="staff_main_card_date">
-            <span>Fri 15</span>
-          </div>
+        {data.map((shift, index) => (
+          <div className={shift.status === "Pending" ? "staff_main_card" : "none"} key={index}>
+            <div className="staff_main_card_date">
+              <span>{formatDate(shift.date)}</span>
+            </div>
 
-          <div className="staff_main_card_info">
-            <span className="shift-location-time">
-              123 Smith Street Sydney, NSW 2000 Australia, 08:00 AM - 11: 00 AM
-            </span>
-            <span>Duration: 10 hours</span>
-          </div>
+            <div className="staff_main_card_info">
+              <span className="shift-location-time">
+                {shift.location}, {shift.time}
+              </span>
+              <span>Duration:{shift.duration}</span>
+            </div>
 
-          <div className="shift_status">
-            <span>Pending</span>
-          </div>
+            <div className="shift_status">
+              <span>{shift.status}</span>
+            </div>
 
-          <div className="staff_main_card_options">
-            <AiOutlineEye size={25} />
+            <div className="staff_main_card_options">
+             <Link to={`/shift/${shift._id}`}>
+             <AiOutlineEye size={25} className="view" />
+             </Link>
+            </div>
           </div>
-        </div>
+        ))}
 
-        <div className="staff_main_card">
-          <div className="staff_main_card_date">
-            <span>Fri 15</span>
-          </div>
-
-          <div className="staff_main_card_info">
-            <span className="shift-location-time">
-              123 Smith Street Sydney, NSW 2000 Australia, 08:00 AM - 11: 00 AM
-            </span>
-            <span>Duration: 10 hours</span>
-          </div>
-
-          <div className="shift_status">
-            <span>Pending</span>
-          </div>
-
-          <div className="staff_main_card_options">
-            <AiOutlineEye size={25} />
-          </div>
-        </div>
-        
         <h3 className="shift_header">Bid Shifts</h3>
+        {unassignedShifts.map((shift, index) => (
+          <div className={shift.status === "Pending" ? "staff_main_card_unassigned" : "none"} key={index}>
+            <div className="staff_main_card_date">
+              <span>{formatDate(shift.date)}</span>
+            </div>
 
-        <div className="staff_main_card_unassigned">
-          <div className="staff_main_card_date">
-            <span>Fri 15</span>
-          </div>
+            <div className="staff_main_card_info">
+              <span className="shift-location-time">
+                {shift.location}, {shift.time}
+              </span>
+              <span>Duration: {shift.duration}</span>
+            </div>
 
-          <div className="staff_main_card_info">
-            <span className="shift-location-time">
-              123 Smith Street Sydney, NSW 2000 Australia, 08:00 AM - 11: 00 AM
-            </span>
-            <span>Duration: 10 hours</span>
-          </div>
+            <div className="shift_status">
+              <span>{shift.status}</span>
+            </div>
 
-          <div className="shift_status">
-            <span>Pending</span>
+            <div className="staff_main_card_options">
+            <Link to={`/shift/${shift._id}`}>
+             <AiOutlineEye size={25} className="view" />
+             </Link>
+            </div>
           </div>
-
-          <div className="staff_main_card_options">
-            <AiOutlineEye size={25} />
-          </div>
-        </div>
-        
+        ))}
       </div>
     </div>
   );
